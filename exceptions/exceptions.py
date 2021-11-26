e36ad9ea3cd5b5a4b0287a3995311a3d976009f0
Almas Gabdrakhimov <12lol12lol12lol12@gmail.com>
2021-11-26 14:56:03 +0500

    module for project exceptions

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,1 +1,19 @@
-from fastapi import HTTPException
+class BaseException(Exception):
+    """
+    Базовое исключение
+    """
+    exception_type = ""
+
+    def __init__(self, *args, **kwargs) -> None:
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def get_message(self):
+        return [
+            {
+                'msg': self.message,
+                'type': self.exception_type
+            }
+        ]
