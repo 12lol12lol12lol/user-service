5e3537cd2a147d49dc9fb0f45dbbff0d4723b5cf
Almas Gabdrakhimov <12lol12lol12lol12@gmail.com>
2021-11-28 18:59:59 +0500

    refactoring

--- db/db.py
+++ db/db.py
@@ -1,7 +1,6 @@
-import motor
-from settings import settings
+import motor.motor_asyncio
 from loguru import logger
-
+from settings import settings
 
 # Initalize database connection
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
