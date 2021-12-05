4b7e278e6cae0be366d19e6a8359b136fd9b66c3
Almas Gabdrakhimov <12lol12lol12lol12@gmail.com>
2021-12-05 19:06:41 +0500

    rename db module

--- db/db_connect.py
+++ db/db_connect.py
@@ -2,7 +2,7 @@ import motor.motor_asyncio
 from loguru import logger
 from settings import settings
 
-# Initalize database connection
+# Initialize database connection
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
 db = client.college
 logger.info('Database initialization completed!')
