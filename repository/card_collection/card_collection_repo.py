34e83c02e6bdd78a311f08b74485307b5c3e1681
Almas Gabdrakhimov <12lol12lol12lol12@gmail.com>
2021-12-07 11:08:42 +0500

    add repository

--- repository/card_collection/card_collection_repo.py
+++ repository/card_collection/card_collection_repo.py
@@ -3,8 +3,8 @@ from models import CardCollection
 
 
 class CardCollectionRepo:
-    db = db
 
-    async def create_card_collection(self, card_collection: CardCollection) -> CardCollection:
+    @staticmethod
+    async def create_card_collection(card_collection: CardCollection) -> CardCollection:
         pass
     
