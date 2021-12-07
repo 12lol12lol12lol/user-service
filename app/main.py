97b558f8f89324433378db74b30c1c4e02577a00
Almas Gabdrakhimov <12lol12lol12lol12@gmail.com>
2021-12-07 17:31:21 +0500

    mount static directory

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routers import collection_router, user_router
 
 # create application
 app = FastAPI()
+
+# mount static directory
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.include_router(router=user_router)
 app.include_router(router=collection_router)
